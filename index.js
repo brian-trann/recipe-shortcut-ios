c5f875eb47abf76d978f36e1dc8e0a154fcd0a9d
brian-trann <70680670+brian-trann@users.noreply.github.com>
2021-07-09 10:13:02 -0700

    add project

---
+++ index.js
@@ -0,0 +1,46 @@
+const elements = document.querySelectorAll('script[type="application/ld+json"]');
+
+function parseElements(elements, result = []) {
+  for (let data of elements) {
+    if (data.innerText) {
+      data = JSON.parse(data.innerText);
+    }
+    if (Array.isArray(data)) {
+      for (let node of data) {
+        if (node['@type'] == 'Recipe') {
+          data = node;
+        }
+      }
+    }
+    if (data.hasOwnProperty('@graph') && Array.isArray(data['@graph'])) {
+      parseElements(data['@graph'], result);
+    }
+    if (data.image) {
+      if (Array.isArray(data.image)) {
+        data['myImgUrl'] = data.image[0].url;
+      }
+      if (data.image.url) {
+        data['myImgUrl'] = data.image.url;
+      }
+    }
+    if (data['@type'] == 'Recipe') {
+      data['myInstructions'] = data.recipeInstructions.map((r, i) => {
+        const step = String(i + 1);
+        const newInstruction = step + '. ' + r.text + '\n';
+        return newInstruction;
+      });
+      for (let i = 0; i < data['recipeIngredient'].length; i++) {
+        data['recipeIngredient'][i] = '* ' + data['recipeIngredient'][i];
+      }
+      result.push(data);
+    }
+  }
+  return result.length > 0 ? result : false;
+}
+const result = parseElements(elements);
+if (!result || !result.length) {
+  completion();
+} else {
+  completion(result);
+}
+// Call completion to finish
